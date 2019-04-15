3988e0c637006a79ca0c91f5ae4c65c06a5f278c
Chantelle Dye <cdye42@gmail.com>
2019-04-15 15:16:27 -0600

    assignment completion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          About Chantelle
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +21,33 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div className="Main">
+          <div id="Part1">
+            <h1>My name is Chantelle Dye.</h1>
+            <span>I am learning web development.</span>
+            <h3>I love learning.</h3>
+          </div>
+          <ol id="Part2">
+            <h1>3 reasons for attending Helio</h1>
+
+            <li>Learning is fun</li>
+
+            <li>Change career path</li>
+
+            <li>Endless opportunities</li>
+          </ol>
+          <div id="Part3">
+            {" "}
+            <h1>My interests are:</h1>
+            <ul>
+              <li>Family</li>
+
+              <li>Outdoors</li>
+
+              <li>Learning</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
